d6eacb76c2d99a447089a59210edc0bc8fbaf0aa
asem-rahsed <asemrashed002@gmail.com>
2025-01-25 09:32:08 +0600

    subrepo

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base:'/restaurat/', 
+  base:'/restaurant/', 
   plugins: [react()],
   optimizeDeps: {
     include: ['@emotion/react', '@emotion/styled'],
